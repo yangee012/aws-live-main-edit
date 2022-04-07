e97a054c556c1eadb5c1c3093b0a59ad05c264ef
yangee012 <limye-wb18@student.tarc.edu.my>
2022-04-07 18:58:05 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -165,11 +165,11 @@ def show_image(bucket):
             # if emp_id in presigned_url:
             #     public_urls.append(presigned_url)
 
-            public_urls.append("https://limyangee-employee.s3.amazonaws.com/upload/1_2_2048x.jpg")
-            print("[INFO]: The content inside show images: ", presigned_url)
+            public_urls.append(presigned_url)
+            
     except Exception as e:
         pass
-    
+    print("[INFO]: The content inside show images: ", presigned_url)
     return public_urls
 
 @app.route("/fetchemp", methods=['POST'])
